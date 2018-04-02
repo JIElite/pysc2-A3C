16f6e376112442b9a5e30e808948cd5d130a4e38
JIElite <ita3051@gmail.com>
2018-04-02 23:57:52 +0800

    add worker func with action type

--- sc2/worker.py
+++ sc2/worker.py
@@ -164,7 +164,7 @@ def worker_non_spatial_spatial(worker_id, args, shared_model, optimizer, global_
             # Reset n-step experience buffer
             entropies = []
             critic_values = []
-            spatial_policy_log_probs = []
+            policy_log_probs = []
             rewards = []
 
 
@@ -179,71 +179,78 @@ def worker_non_spatial_spatial(worker_id, args, shared_model, optimizer, global_
 
                 non_spatial_action = non_spatial_action_prob.multinomial()
                 spatial_action = spatial_action_prob.multinomial()
+
+                non_spatial_entropy = -(non_spatial_action_log_prob * non_spatial_action_prob).sum(1)
                 spatial_entropy = -(log_spatial_action_prob * spatial_action_prob).sum(1)
-                selected_log_action_prob = log_spatial_action_prob.gather(1, spatial_action)
+                entropy = non_spatial_entropy + spatial_entropy
+
+                selected_log_non_spatial_action_prob = non_spatial_action_log_prob.gather(1, non_spatial_action)
+                selected_log_spatial_action_prob = log_spatial_action_prob.gather(1, spatial_action)
+                selected_action_log_prob = selected_log_non_spatial_action_prob + selected_log_spatial_action_prob
 
                 # record n-step experience
-                # TODO entropy = spatial_entropy + non_spatial_entropy
-                entropies.append(spatial_entropy)
-                spatial_policy_log_probs.append(selected_log_action_prob)
+                entropies.append(entropy)
+                policy_log_probs.append(selected_action_log_prob)
                 critic_values.append(value)
 
+
+                action_id = int(non_spatial_action.data.cpu().numpy())
+                action_type = _NO_OP if action_id == 0 else _MOVE_SCREEN
+
                 # Step
-                action = game_inferface.build_action(_MOVE_SCREEN, spatial_action[0].cpu())
+                action = game_inferface.build_action(action_type, spatial_action[0].cpu())
                 state = env.step([action])[0]
 
+                reward = np.asscalar(state.reward)
+                rewards.append(reward)
+                episode_reward += reward
 
-            #
-            #     reward = np.asscalar(state.reward)
-            #     rewards.append(reward)
-            #     episode_reward += reward
-            #
-            #     episode_length += 1
-            #     global_counter.value += 1
-            #
-            #
-            #     episode_done = (episode_length >= args['max_eps_length']) or state.last()
-            #     if episode_done:
-            #         episode_length = 0
-            #         env.reset()
-            #         state = env.step([actions.FunctionCall(_NO_OP, [])])[0]
-            #         break
-            #
-            # R_t = torch.zeros(1)
-            # if not episode_done:
-            #     screen_observation = Variable(torch.from_numpy(game_inferface.get_screen_obs(
-            #                                                         timesteps=state,
-            #                                                         indexes=[4, 5, 6, 7, 8, 9, 14, 15]
-            #                                                 ))).cuda()
-            #     _, _, value = local_model(screen_observation)
-            #     R_t = value.data
-            #
-            #
-            # R_var = Variable(R_t).cuda()
-            # critic_values.append(R_var)
-            # policy_loss = 0.
-            # value_loss = 0.
-            # gae_ts = torch.zeros(1).cuda()
-            # for i in reversed(range(len(rewards))):
-            #     R_var = rewards[i] + args['gamma'] * R_var
-            #
-            #     advantage_var = R_var - critic_values[i]
-            #     value_loss += 0.5 * advantage_var.pow(2)
-            #
-            #     td_error = rewards[i] + args['gamma'] * critic_values[i+1].data - critic_values[i].data
-            #     gae_ts = gae_ts * args['gamma'] * args['tau'] + td_error
-            #     policy_loss += -(spatial_policy_log_probs[i] * Variable(gae_ts, requires_grad=False) + 0.05 *entropies[i])
-            #
-            # optimizer.zero_grad()
-            # total_loss = policy_loss + 0.5 * value_loss
-            # total_loss.backward()
-            # torch.nn.utils.clip_grad_norm(local_model.parameters(), 40)
-            # ensure_shared_grad(local_model, shared_model)
-            # optimizer.step()
-            #
-            # if episode_done:
-            #     summary_queue.put((global_counter.value, episode_reward))
-            #     episode_reward = 0
-            #
+                episode_length += 1
+                global_counter.value += 1
+
+
+                episode_done = (episode_length >= args['max_eps_length']) or state.last()
+                if episode_done:
+                    episode_length = 0
+                    env.reset()
+                    state = env.step([actions.FunctionCall(_NO_OP, [])])[0]
+                    break
+
+            R_t = torch.zeros(1)
+            if not episode_done:
+                screen_observation = Variable(torch.from_numpy(game_inferface.get_screen_obs(
+                                                                    timesteps=state,
+                                                                    indexes=[4, 5, 6, 7, 8, 9, 14, 15]
+                                                            ))).cuda()
+                _, _, _, _, value = local_model(screen_observation)
+                R_t = value.data
+
+
+            R_var = Variable(R_t).cuda()
+            critic_values.append(R_var)
+            policy_loss = 0.
+            value_loss = 0.
+            gae_ts = torch.zeros(1).cuda()
+            for i in reversed(range(len(rewards))):
+                R_var = rewards[i] + args['gamma'] * R_var
+
+                advantage_var = R_var - critic_values[i]
+                value_loss += 0.5 * advantage_var.pow(2)
+
+                td_error = rewards[i] + args['gamma'] * critic_values[i+1].data - critic_values[i].data
+                gae_ts = gae_ts * args['gamma'] * args['tau'] + td_error
+                policy_loss += -(policy_log_probs[i] * Variable(gae_ts, requires_grad=False) + 0.05 *entropies[i])
+
+            optimizer.zero_grad()
+            total_loss = policy_loss + 0.5 * value_loss
+            total_loss.backward()
+            torch.nn.utils.clip_grad_norm(local_model.parameters(), 40)
+            ensure_shared_grad(local_model, shared_model)
+            optimizer.step()
+
+            if episode_done:
+                summary_queue.put((global_counter.value, episode_reward))
+                episode_reward = 0
+
 
 
