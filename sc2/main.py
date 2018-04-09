8d9ad223d1f49df9d69fcb04eb1ef644c542f574
JIElite <ita3051@gmail.com>
2018-04-09 22:47:41 +0800

    update settings

--- sc2/main.py
+++ sc2/main.py
@@ -5,6 +5,7 @@ os.environ['OMP_NUM_THREADS']='1'
 from absl import app, flags
 from pysc2 import maps
 from pysc2.env import sc2_env
+import torch
 import torch.multiprocessing as mp
 
 from model import FullyConv
@@ -15,14 +16,14 @@ from monitor import evaluator
 
 FLAGS = flags.FLAGS
 # Game related settings
-flags.DEFINE_string("map", "CollectMineralShardsSingle", "Name of a map to use.")
+flags.DEFINE_string("map", "CollectMineralShardsSingleExtended", "Name of a map to use.")
 flags.DEFINE_integer("screen_resolution", 32, "Resolution for screen feature layers.")
 flags.DEFINE_integer("minimap_resolution", 32, "Resolution for minimap feature layers.")
 flags.DEFINE_bool("visualize", False, "Whether to render with pygame.")
 flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
 flags.DEFINE_enum("agent_race", None, sc2_env.races.keys(), "Agent's race.")
 flags.DEFINE_enum("bot_race", None, sc2_env.races.keys(), "Bot's race.")
-flags.DEFINE_integer('total_steps', 10500000, "steps run for each worker")
+flags.DEFINE_integer('worker_steps', 10500000, "steps run for each worker")
 flags.DEFINE_integer('max_eps_length', 5000, "max length run for each episode")
 
 # Learning related settings
@@ -45,12 +46,15 @@ def main(argv):
 
     # share model
     shared_model = FullyConv(screen_channels=8, screen_resolution=(FLAGS.screen_resolution, FLAGS.screen_resolution)).cuda()
+    shared_model.load_state_dict(torch.load('./models/task1_extended_no_action_id_15264524/model_best'))
+
     shared_model.share_memory()
     optimizer = SharedAdam(shared_model.parameters(), lr=FLAGS.learning_rate)
     optimizer.share_memory()
 
     worker_list = []
-    evaluate_worker = mp.Process(target=evaluator, args=(summary_queue, shared_model, global_counter))
+    evaluate_worker = mp.Process(target=evaluator, args=(summary_queue, shared_model, global_counter,
+                                                         FLAGS.num_of_workers*FLAGS.worker_steps))
     evaluate_worker.start()
     worker_list.append(evaluate_worker)
 
