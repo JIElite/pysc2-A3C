6594987557b7cb89cef2f4084bada6b0b4a084fd
JIElite <ita3051@gmail.com>
2018-04-15 22:43:29 +0800

    add code to train 3-head model, choose unit to collect minerals in a single model

--- sc2/model.py
+++ sc2/model.py
@@ -73,6 +73,48 @@ class FullyConvExtended(nn.Module):
         return action_prob, value
 
 
+class FullyConvMultiUnitCollectBaseline(nn.Module):
+    def __init__(self, screen_channels, screen_resolution):
+        super(FullyConvMultiUnitCollectBaseline, self).__init__()
+        self.conv1 = nn.Conv2d(screen_channels, 16, kernel_size=(5, 5), stride=1, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.spatial_policy = nn.Conv2d(32, 1, kernel_size=(1, 1))
+        self.select_unit = nn.Conv2d(32, 1, kernel_size=(1, 1))
+
+        self.non_spatial_branch = nn.Linear(screen_resolution[0] * screen_resolution[1] * 32, 256)
+        self.value = nn.Linear(256, 1)
+
+        # init weight
+        nn.init.xavier_uniform(self.conv1.weight.data)
+        nn.init.xavier_uniform(self.conv2.weight.data)
+        nn.init.xavier_uniform(self.spatial_policy.weight.data)
+        nn.init.xavier_uniform(self.select_unit.weight.data)
+        nn.utils.weight_norm(self.non_spatial_branch)
+        nn.utils.weight_norm(self.value)
+        self.non_spatial_branch.bias.data.fill_(0)
+        self.value.bias.data.fill_(0)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+
+        # select unit branch
+        select_unit_branch = self.select_unit(x)
+        select_unit_branch = select_unit_branch.view(select_unit_branch.shape[0], -1)
+        select_unit_prob =  nn.functional.softmax(select_unit_branch, dim=1)
+
+        # spatial policy branch
+        policy_branch = self.spatial_policy(x)
+        policy_branch = policy_branch.view(policy_branch.shape[0], -1)
+        spatial_action_prob = nn.functional.softmax(policy_branch, dim=1)
+
+        # non spatial branch
+        non_spatial_represenatation = F.relu(self.non_spatial_branch(x.view(-1))) # flatten the state representation
+        value = self.value(non_spatial_represenatation)
+
+        return select_unit_prob, spatial_action_prob, value
+
+
 class FullyConvSelecAction(nn.Module):
     def __init__(self, screen_channels, screen_resolution):
         super(FullyConvSelecAction, self).__init__()
--- sc2/train_hierarchical.py
+++ sc2/train_hierarchical.py
@@ -11,7 +11,7 @@ from pysc2.lib import features
 
 from envs import GameInterfaceHandler
 from envs import create_pysc2_env
-from model import FullyConv
+from model import FullyConv, FullyConvMultiUnitCollectBaseline
 from optimizer import ensure_shared_grad
 
 
@@ -165,3 +165,141 @@ def train_master(worker_id, args, shared_model, optimizer, global_counter, summa
             if episode_done:
                 summary_queue.put((global_counter.value, episode_reward))
                 episode_reward = 0
+
+
+def train_hierarchical_collect_baseline(
+        worker_id, args, shared_model, optimizer, global_counter, summary_queue):
+
+        torch.manual_seed(args['seed'] + worker_id)
+        env_args = {
+            'map_name': args['map'],
+            'agent_race': args['agent_race'],
+            'bot_race': args['bot_race'],
+            'step_mul': args['step_mul'],
+            'screen_size_px': [args['screen_resolution']] * 2,
+            'minimap_size_px': [args['minimap_resolution']] * 2,
+            'visualize': args['visualize'],
+        }
+        env = create_pysc2_env(env_args)
+        game_inferface = GameInterfaceHandler(screen_resolution=args['screen_resolution'],
+                                              minimap_resolution=args['minimap_resolution'])
+        with env:
+            local_model = FullyConvMultiUnitCollectBaseline(
+                screen_channels=8, screen_resolution=[args['screen_resolution']] * 2).cuda(args['gpu'])
+
+            env.reset()
+            state = env.step([actions.FunctionCall(_NO_OP, [])])[0]
+            episode_done = False
+            episode_length = 0
+            episode_reward = 0
+
+            while True:
+                time.sleep(0.1)
+                # Sync the parameters with shared model
+                local_model.load_state_dict(shared_model.state_dict())
+
+                # Reset n-step experience buffer
+                entropies = []
+                critic_values = []
+                chosen_select_policy_log_probs = []
+                chosen_spatial_policy_log_probs = []
+                rewards = []
+
+                # step forward n steps
+                for step in range(args['n_steps']):
+                    screen_observation = Variable(torch.from_numpy(game_inferface.get_screen_obs(
+                        timesteps=state,
+                        indexes=[4, 5, 6, 7, 8, 9, 14, 15],
+                    ))).cuda(args['gpu'])
+
+                    select_action_prob, spatial_action_prob, value = local_model(screen_observation)
+
+                    # mask select action
+                    selection_mask = torch.from_numpy(
+                        (state.observation['screen'][_SCREEN_PLAYER_RELATIVE] == 1).astype('float32'))
+                    selection_mask = Variable(selection_mask.view(1, -1), requires_grad=False).cuda(args['gpu'])
+                    masked_select_action_prob = select_action_prob * selection_mask
+                    masked_select_action_prob = masked_select_action_prob / masked_select_action_prob.sum()
+                    # m = Categorical(masked_select_action_prob)
+                    # select_unit_action = m.sample().unsqueeze(0)
+                    select_unit_action = masked_select_action_prob.multinomial()
+                    select_action = game_inferface.build_action(_SELECT_POINT, select_unit_action[0].cpu())
+
+                    spatial_action = spatial_action_prob.multinomial()
+                    move_action = game_inferface.build_action(_MOVE_SCREEN, spatial_action[0].cpu())
+
+                    log_select_action_prob = torch.log(
+                        torch.clamp(masked_select_action_prob, min=1e-12))
+                    log_spatial_action_prob = torch.log(
+                        torch.clamp(spatial_action_prob, min=1e-12))
+
+                    # compute entropy?
+                    select_entropy = - (log_select_action_prob * masked_select_action_prob).sum(1)
+                    spatial_entropy = - (log_spatial_action_prob * spatial_action_prob).sum(1)
+                    entropy = select_entropy + spatial_entropy
+
+                    chosen_log_select_unit_prob = log_select_action_prob.gather(1, select_unit_action)
+                    chosen_log_spatial_action_prob = log_spatial_action_prob.gather(1, spatial_action)
+
+                    # Step
+                    state = env.step([select_action])[0]
+                    reward = np.asscalar(state.reward)
+                    state = env.step([move_action])[0]
+                    reward += np.asscalar(state.reward)
+
+                    # record n-step data
+                    entropies.append(entropy)
+                    chosen_select_policy_log_probs.append(chosen_log_select_unit_prob)
+                    chosen_spatial_policy_log_probs.append(chosen_log_spatial_action_prob)
+                    rewards.append(reward)
+                    critic_values.append(value)
+
+                    # update statistical information
+                    episode_reward += reward
+                    episode_length += 1
+                    global_counter.value += 1
+
+                    episode_done = (episode_length >= args['max_eps_length']) or state.last()
+                    if episode_done:
+                        episode_length = 0
+                        env.reset()
+                        state = env.step([actions.FunctionCall(_NO_OP, [])])[0]
+                        break
+
+                # TODO update model, compute gradient
+                R_t = torch.zeros(1)
+                if not episode_done:
+                    screen_observation = Variable(torch.from_numpy(game_inferface.get_screen_obs(
+                        timesteps=state,
+                        indexes=[4, 5, 6, 7, 8, 9, 14, 15]
+                    ))).cuda(args['gpu'])
+                    _, _, value = local_model(screen_observation)
+                    R_t = value.data
+
+                R_var = Variable(R_t).cuda(args['gpu'])
+                critic_values.append(R_var)
+                policy_loss = 0.
+                value_loss = 0.
+                gae_ts = torch.zeros(1).cuda(args['gpu'])
+                for i in reversed(range(len(rewards))):
+                    R_var = rewards[i] + args['gamma'] * R_var
+
+                    advantage_var = R_var - critic_values[i]
+                    value_loss += 0.5 * advantage_var.pow(2)
+
+                    td_error = rewards[i] + args['gamma'] * critic_values[i + 1].data - critic_values[i].data
+                    gae_ts = gae_ts * args['gamma'] * args['tau'] + td_error
+
+                    policy_log_for_action = chosen_select_policy_log_probs[i] + chosen_spatial_policy_log_probs[i]
+                    policy_loss += -(policy_log_for_action * Variable(gae_ts, requires_grad=False) + 0.05 * entropies[i])
+
+                optimizer.zero_grad()
+                total_loss = policy_loss + 0.5 * value_loss
+                total_loss.backward()
+                torch.nn.utils.clip_grad_norm(local_model.parameters(), 40)
+                ensure_shared_grad(local_model, shared_model)
+                optimizer.step()
+
+                if episode_done:
+                    summary_queue.put((global_counter.value, episode_reward))
+                    episode_reward = 0
