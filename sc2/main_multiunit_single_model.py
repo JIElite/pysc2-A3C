f3ea7a0f441ec4612cc92331291a9666d09dc934
JIElite <ita3051@gmail.com>
2018-04-18 12:43:07 +0800

    update flags: gpu, postfix, use extend model or not

--- sc2/main_multiunit_single_model.py
+++ sc2/main_multiunit_single_model.py
@@ -10,9 +10,10 @@ os.environ['OMP_NUM_THREADS']='1'
 from absl import app, flags
 from pysc2 import maps
 from pysc2.env import sc2_env
+import torch
 import torch.multiprocessing as mp
 
-from model import FullyConvMultiUnitCollectBaseline
+from model import FullyConvMultiUnitCollectBaseline, FullyConvMultiUnitCollectBaselineExtended
 from optimizer import SharedAdam
 from monitor import evaluator
 from train_hierarchical import train_hierarchical_collect_baseline
@@ -37,8 +38,14 @@ flags.DEFINE_integer("n_steps", 8,  "How many steps do we compute the Return (TD
 flags.DEFINE_integer("seed", 5, "torch random seed")
 flags.DEFINE_float("tau", 1.0, "tau for GAE")
 flags.DEFINE_boolean("multiple_gpu", False, "use multiple gpu or single gpu")
-flags.DEFINE_integer("gpu", 1, "gpu device")
+flags.DEFINE_integer("gpu", 0, "gpu device")
+flags.DEFINE_boolean("extend_model", False, "using extended model or not")
+# statistical postfix
+flags.DEFINE_string("postfix", "", "postfix of training data")
 FLAGS(sys.argv)
+
+torch.cuda.set_device(FLAGS.gpu)
+print("CUDA device:", torch.cuda.current_device())
 
 
 def main(argv):
@@ -48,14 +55,19 @@ def main(argv):
     global_counter = mp.Value('i', 0)
     summary_queue = mp.Queue()
 
+    if FLAGS.extend_model:
+        model = FullyConvMultiUnitCollectBaselineExtended
+    else:
+        model = FullyConvMultiUnitCollectBaseline
+
     # share model
     use_multiple_gpu = FLAGS.multiple_gpu
     if use_multiple_gpu:
-        shared_model = FullyConvMultiUnitCollectBaseline(screen_channels=8, screen_resolution=(
+        shared_model = model(screen_channels=8, screen_resolution=(
             FLAGS.screen_resolution, FLAGS.screen_resolution))
     else:
-        shared_model = FullyConvMultiUnitCollectBaseline(screen_channels=8, screen_resolution=(
-            FLAGS.screen_resolution, FLAGS.screen_resolution)).cuda(0)
+        shared_model = model(screen_channels=8, screen_resolution=(
+            FLAGS.screen_resolution, FLAGS.screen_resolution)).cuda(FLAGS.gpu)
 
     shared_model.share_memory()
     optimizer = SharedAdam(shared_model.parameters(), lr=FLAGS.learning_rate)
@@ -65,7 +77,7 @@ def main(argv):
     evaluate_worker = mp.Process(target=evaluator, args=(summary_queue, shared_model, optimizer,
                                                          global_counter,
                                                          FLAGS.num_of_workers * FLAGS.worker_steps,
-                                                         ''))
+                                                         FLAGS.postfix))
     evaluate_worker.start()
     worker_list.append(evaluate_worker)
 
