a0e866726b012d545ca44665e374e1c08dde9e3d
JIElite <ita3051@gmail.com>
2018-04-18 12:42:05 +0800

    add extended baseline

--- sc2/model.py
+++ sc2/model.py
@@ -51,6 +51,7 @@ class FullyConvExtended(nn.Module):
         # init weight
         nn.init.xavier_uniform(self.conv1.weight.data)
         nn.init.xavier_uniform(self.conv2.weight.data)
+        nn.init.xavier_uniform(self.conv3.weight.data)
         nn.init.xavier_uniform(self.spatial_policy.weight.data)
         nn.utils.weight_norm(self.non_spatial_branch)
         nn.utils.weight_norm(self.value)
@@ -97,6 +98,51 @@ class FullyConvMultiUnitCollectBaseline(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+
+        # select unit branch
+        select_unit_branch = self.select_unit(x)
+        select_unit_branch = select_unit_branch.view(select_unit_branch.shape[0], -1)
+        select_unit_prob = nn.functional.softmax(select_unit_branch, dim=1)
+
+        # spatial policy branch
+        policy_branch = self.spatial_policy(x)
+        policy_branch = policy_branch.view(policy_branch.shape[0], -1)
+        spatial_action_prob = nn.functional.softmax(policy_branch, dim=1)
+
+        # non spatial branch
+        non_spatial_represenatation = F.relu(self.non_spatial_branch(x.view(-1))) # flatten the state representation
+        value = self.value(non_spatial_represenatation)
+
+        return select_unit_prob, spatial_action_prob, value
+
+
+class FullyConvMultiUnitCollectBaselineExtended(nn.Module):
+    def __init__(self, screen_channels, screen_resolution):
+        super(FullyConvMultiUnitCollectBaselineExtended, self).__init__()
+        self.conv1 = nn.Conv2d(screen_channels, 16, kernel_size=(5, 5), stride=1, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.spatial_policy = nn.Conv2d(32, 1, kernel_size=(1, 1))
+        self.select_unit = nn.Conv2d(32, 1, kernel_size=(1, 1))
+
+        self.non_spatial_branch = nn.Linear(screen_resolution[0] * screen_resolution[1] * 32, 256)
+        self.value = nn.Linear(256, 1)
+
+        # init weight
+        nn.init.xavier_uniform(self.conv1.weight.data)
+        nn.init.xavier_uniform(self.conv2.weight.data)
+        nn.init.xavier_uniform(self.conv3.weight.data)
+        nn.init.xavier_uniform(self.spatial_policy.weight.data)
+        nn.init.xavier_uniform(self.select_unit.weight.data)
+        nn.utils.weight_norm(self.non_spatial_branch)
+        nn.utils.weight_norm(self.value)
+        self.non_spatial_branch.bias.data.fill_(0)
+        self.value.bias.data.fill_(0)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
 
         # select unit branch
         select_unit_branch = self.select_unit(x)
