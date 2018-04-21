f5b9c4e7c4c5c5886e938a8dcf4d42b2e1c55f47
JIElite <ita3051@gmail.com>
2018-04-21 19:37:27 +0800

    update show statistics, add steps option

--- sc2/show_statistics.py
+++ sc2/show_statistics.py
@@ -1,3 +1,7 @@
+"""
+usage:
+python show_statistics.py --input statistics/collect_five_nj_extend_conv3_14045348.pkl --steps 400000
+"""
 import pickle
 import matplotlib.pyplot as plt
 import argparse
@@ -5,14 +9,21 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', dest='source', default='statistics.pkl', help='input of statistics file')
+parser.add_argument('--steps', dest='steps', default=None, help='display to num_of_steps')
 args = parser.parse_args()
+
 
 with open(args.source, 'rb') as fd:
     data = pickle.load(fd)
 
-plt.plot(data['mean_perf'])
-plt.plot(data['recent_100_mean_perf'])
-plt.plot(data['recent_100_best_mean_perf'])
+if args.steps is None:
+    end_of_steps = len(data['mean_perf'])
+else:
+    end_of_steps = int(int(args.steps) / 10000)
+
+plt.plot(data['mean_perf'][:end_of_steps])
+plt.plot(data['recent_100_mean_perf'][:end_of_steps])
+plt.plot(data['recent_100_best_mean_perf'][:end_of_steps])
 plt.ylabel('average return')
 plt.xlabel('steps (1e4)')
 plt.show()
